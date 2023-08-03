8358117d968afb2efc1f244beb271938c78c27dc
randima <adrandima@gmail.com>
2023-08-04 00:04:07 +0530

    add comment

--- server/src/router/authorRouter.js
+++ server/src/router/authorRouter.js
@@ -9,9 +9,16 @@ import {
 import { authMiddleware } from "../controllers/authController";
 const authorRouter = express.Router();
 
+// Route to handle GET request for all authors.
 authorRouter.get("/authors", authMiddleware, getAuthors);
+
+// Route to handle GET request for a specific author by ID.
 authorRouter.get("/author/:id", authMiddleware, getAuthorById);
+
+// Route to handle POST request for creating a new author.
 authorRouter.post("/author", authMiddleware, createAuthor);
+
+// Route to handle PUT request for updating an existing author by ID.
 authorRouter.put("/author/:id", authMiddleware, updateAuthor);
 
 export { authorRouter };
