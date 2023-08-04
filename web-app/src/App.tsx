fe9c94a7f93977cf0f5a3f64172c107de65ed429
randima <adrandima@gmail.com>
2023-08-04 14:43:46 +0530

    handle navigation

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,10 +1,68 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav } from "react-bootstrap";
+import "./assets/components/index.css";
+import "./assets/components/signInUp.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+
+import SignInUp from "./containers/User/signInUp";
+import Book from "./containers/Book/book";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/sign");
+    }
+  }, [navigate]);
+
+  const handleSignOut = () => {
+    // Remove the JWT token from localStorage
+    localStorage.removeItem("token");
+
+    // Redirect to the sign-in page
+    navigate("/sign");
+  };
+
   return (
     <div className="App">
-      
+      <Navbar expand="lg" fixed="top" bg="light" variant="light">
+        <Navbar.Brand as={Link} to={"/book"}>
+          Home
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarToggler" />
+        <Navbar.Collapse id="navbarToggler">
+          <Nav className="ml-auto">
+            {localStorage.getItem("token") ? (
+              <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link>
+            ) : (
+              <>
+                <Nav.Link as={Link} to={"/sign-in"}>
+                  Login
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <div className="auth-wrapper">
+        <Routes>
+          <Route path="/" element={<SignInUp />} />
+          <Route path="/sign" element={<SignInUp />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="*" element={null} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./assets/components/app.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import store from "./store";
+
+const appElement = document.getElementById("root");
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  appElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
